864200282940741842a3c55c38c3e67ab403d0f6
Dharmendra Singh <dharmendra.singh@pegasusone.com>
2020-01-15 10:20:48 +0530

    Initial Commit

---
+++ DataBinding/UserControls/RootControl.xaml.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace DataBinding.UserControls
+{
+	public sealed partial class RootControl : UserControl, INotifyPropertyChanged
+	{
+		public RootControl()
+		{
+			this.InitializeComponent();
+		}
+		private double _btnWidth = 50;
+
+		public double Radius
+		{
+			get { return _btnWidth; }
+
+			set
+			{
+				_btnWidth = value;
+				if (PropertyChanged != null)
+				{
+					PropertyChanged(this, new PropertyChangedEventArgs("Radius"));
+				}
+			}
+		}
+		public event PropertyChangedEventHandler PropertyChanged;
+	}
+}
---
+++ DataBinding/ViewModel/ChildViewModel.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Template10.Mvvm;
+
+namespace DataBinding.ViewModel
+{
+	public class ChildViewModel:ViewModelBase
+	{
+		private int _sliderValue = 25;
+		public int EllipseDimention
+		{
+			get
+			{
+				return _sliderValue;
+			}
+			set
+			{
+				Set(ref _sliderValue, value, "SliderValue");
+			}
+		}
+	
+		public int SliderValue
+		{
+			get
+			{
+				return _sliderValue;
+			}
+			set
+			{
+				Set(ref _sliderValue, value, "EllipseDimention");			
+
+			}
+		}
+	}
+}
